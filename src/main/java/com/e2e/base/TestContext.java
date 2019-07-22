0b86f42fe115c3b8a0d6a679996527ef07ec4a14
Rahul Sahu <rahulsahu@PUN-IT000501.local>
2019-07-22 12:33:25 +0530

    Signed-off-by: Rahul Sahu <rahulsahu@PUN-IT000501.local>

--- src/main/java/com/e2e/base/TestContext.java
+++ src/main/java/com/e2e/base/TestContext.java
@@ -2,24 +2,14 @@ package com.e2e.base;
 
 public class TestContext {
 
-	public static final String USERNAME = "premiumautomatio1";
 
-	public static final String AUTOMATE_KEY = "syY7EGda6EismVezbmxs";
+    public static String baseUrl;
 
-	public static final String browserURL = "https://" + USERNAME + ":" + AUTOMATE_KEY
-			+ "@hub-cloud.browserstack.com/wd/hub";
+    public static String userEmail;
 
-	public static String baseUrl;
+    public static String userPassword;
 
-	public static String userEmail;
-
-	public static String userPassword;
-
-	public static String productName;
-
-	public static String isNewUI;
-
-	public static String TIMEOUT = "20000";
+    public static String TIMEOUT = "20000";
 
 
 }
